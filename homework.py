8d90f9f71c47ca29265b9c21e67ff0a376204ffe
Contrigra <gaponov60b@gmail.com>
2020-04-06 21:59:36 +0300

    Made a proper record class. Autoformatting time strings and requests current time.

--- homework.py
+++ homework.py
@@ -6,31 +6,43 @@ class Calculator:
         self.limit = limit
         self.records = []
 
+    def add_record(self,record_class):
+        self.records.append([record_class.amount, record_class.comment, record_class.date])
+
+
+
 class CaloriesCalculator(Calculator):
     pass
 
 class CashCalculator(Calculator):
-    pass
+    123
 
 
 class Record(Calculator):
-
-    def __init__(self, amount, comment, date):
+    def __init__(self, amount, comment, date = None):
+        self.date_format = "%d.%m.%Y"
         self.amount = amount
         self.comment = comment
-        self.date = date #TODO make current time through datetime
-        super().records = records #Как-то надо сделать рекордсы
+        self.date = date
+        # Checking the data type of date.
+        if date is None:        # Requesting current time and then transform it into a string with given date_format
+            self.date = dt.datetime.strftime(dt.datetime.now(), self.date_format)
+        else:                   # if we get a string of time, reformat it into the given format (strptime formats the string into datetime-object, so we reformat it again in a proper string)
+            self.date = dt.datetime.strftime(dt.datetime.strptime(date, self.date_format), self.date_format)
 
-    def add_record(self):
-        self.records.append([self.amount, self.comment, self.date])
 
 
 
 
-calculator1 = Calculator(1500)
-r1 = Record(145, "lol", 2019)
+cash_calculator = CashCalculator(1000)
 
-r1.add_record()
+# дата в параметрах не указана,
+# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment="кофе"))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
 
-print(calculator1.records)
 
+print(cash_calculator.records)
