b02b293d268c17a89efb4fefa019213ad29ef28c
Contrigra <gaponov60b@gmail.com>
2020-04-07 20:06:02 +0300

    Finished all classes. Sending it to the reviewer

--- homework.py
+++ homework.py
@@ -6,16 +6,72 @@ class Calculator:
         self.limit = limit
         self.records = []
 
-    def add_record(self,record_class):
-        self.records.append([record_class.amount, record_class.comment, record_class.date])
+    def add_record(self, record_class):
+        self.records.append(record_class)
 
+    def get_today_stats(self):
+        self.today_stats = 0                                                            #Creating a variable
+        for self.record in self.records:
+            if self.record.date == dt.datetime.now().date():                            #Checking if the date is today.
+                self.today_stats += self.record.amount
+        return self.today_stats                                                         #Returning stats of spent money for today.
+
+    def get_week_stats(self):
+        self.week_amount = 0
+        self.date_now = dt.datetime.now()
+        self.date_week_ago = self.date_now - dt.timedelta(days = 7) #узнаём, время неделю назад
+        for self.record in self.records:
+            if self.record.date > self.date_week_ago.date():
+                self.week_amount += self.record.amount
+        return self.week_amount
 
 
 class CaloriesCalculator(Calculator):
-    pass
+    def get_calories_remained(self):
+        self.limit_left = 0
+        self.calories_eaten = 0
+        self.date_now = dt.datetime.now().date()
+        for self.record in self.records:
+            if self.record.date == self.date_now:
+                self.calories_eaten += self.record.amount
+        self.limit_left = self.limit - self.calories_eaten
+        if self.limit_left > 0:
+            return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit_left} кКал"
+        else:
+            return f"Хватит есть!"
+
 
 class CashCalculator(Calculator):
-    123
+    EURO_RATE = 70.0
+    USD_RATE = 60.0
+    def get_today_cash_remained(self, currency = None):
+        self.spent_money = self.get_today_stats()
+        self.cash_remainder = self.limit - self.spent_money #Calculating the remainder
+        self.cash_remainder = float(self.cash_remainder) #Transforming remainder into float, if we get a non-float
+        #branching, for different currencies
+        if currency == "rub":
+            if self.cash_remainder > 0:
+                return f"На сегодня осталось {round(self.cash_remainder, 2)} руб"
+            elif self.cash_remainder == 0:
+                return f"Денег нет, держись"
+            elif self.cash_remainder < 0:
+                return f"Денег нет, держись: твой долг - {abs(round(self.cash_remainder, 2))} руб"
+        elif currency == "eur":
+            self.cash_remainder = self.cash_remainder / self.EURO_RATE
+            if self.cash_remainder > 0:
+                return f"На сегодня осталось {round(self.cash_remainder, 2)} Euro"
+            elif self.cash_remainder == 0:
+                return f"Денег нет, держись"
+            elif self.cash_remainder < 0:
+                return f"Денег нет, держись: твой долг - {abs(round(self.cash_remainder, 2))} Euro"
+        elif currency == "usd":
+            self.cash_remainder = self.cash_remainder / self.USD_RATE
+            if self.cash_remainder > 0:
+                return f"На сегодня осталось {round(self.cash_remainder, 2)} USD"
+            elif self.cash_remainder == 0:
+                return f"Денег нет, держись"
+            elif self.cash_remainder < 0:
+                return f"Денег нет, держись: твой долг - {abs(round(self.cash_remainder, 2))} USD"
 
 
 class Record(Calculator):
@@ -25,24 +81,11 @@ class Record(Calculator):
         self.comment = comment
         self.date = date
         # Checking the data type of date.
-        if date is None:        # Requesting current time and then transform it into a string with given date_format
-            self.date = dt.datetime.strftime(dt.datetime.now(), self.date_format)
-        else:                   # if we get a string of time, reformat it into the given format (strptime formats the string into datetime-object, so we reformat it again in a proper string)
-            self.date = dt.datetime.strftime(dt.datetime.strptime(date, self.date_format), self.date_format)
+        if date is None:        #Request current time and transform it via .date method to the suitable format
+            self.date = (dt.datetime.now()).date()
+        else:                   # if we get a string of time, reformat it into the given format using strptime.
+                                #.date transform it into suitable format of datetime-type
+            self.date = (dt.datetime.strptime(self.date, self.date_format)).date()
 
 
 
-
-
-cash_calculator = CashCalculator(1000)
-
-# дата в параметрах не указана,
-# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment="кофе"))
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-
-
-print(cash_calculator.records)
