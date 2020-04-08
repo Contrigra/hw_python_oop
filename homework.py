a2f9f52e67f96d8358840ecbed6fe7db784de5bf
Contrigra <gaponov60b@gmail.com>
2020-04-08 19:54:40 +0300

    Fixed all PEP8,
    changed CashCalculator: added error-check using currency format. Added default currency. Created new variables for readability

--- homework.py
+++ homework.py
@@ -20,7 +20,7 @@ class Calculator:
     def get_week_stats(self):
         week_amount = 0
         date_now = dt.datetime.now()
-        date_week_ago = date_now - dt.timedelta(weeks=1)  # Learning time a week ago
+        date_week_ago = date_now - dt.timedelta(weeks=1)  # time a week ago
         for record in self.records:
             if record.date > date_week_ago.date():
                 week_amount += record.amount
@@ -32,7 +32,8 @@ class CaloriesCalculator(Calculator):
         calories_eaten = self.get_today_stats()  # Requesting stats for today
         limit_left = self.limit - calories_eaten
         if limit_left > 0:
-            return f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {limit_left} кКал"
+            return f"Сегодня можно съесть что-нибудь ещё, но с" \
+                   f" общей калорийностью не более {limit_left} кКал"
         else:
             return f"Хватит есть!"
 
@@ -41,18 +42,31 @@ class CashCalculator(Calculator):
     EURO_RATE = 70.0
     USD_RATE = 60.0
     RUB_RATE = 1
-    currency_data = {"eur": (EURO_RATE, "Euro"), "usd": (USD_RATE, "USD"), "rub": (RUB_RATE, "руб")}
+    currency_data = {
+        "eur": (EURO_RATE, "Euro"),
+        "usd": (USD_RATE, "USD"),
+        "rub": (RUB_RATE, "руб")
+    }
 
-    def get_today_cash_remained(self, currency):
+    def get_today_cash_remained(self, currency=None):
+        if currency not in self.currency_data:
+            if currency is None:
+                currency = "rub"
+            else:
+                return f"Курс не поддерживается"
+        currency_rate = self.currency_data.get(currency)[0]
+        currency_name = self.currency_data.get(currency)[1]
         spent_money = self.get_today_stats()
-        cash_remainder = (self.limit - spent_money) / self.currency_data[currency][0]  # Calculating the remainder
-        cash_remainder = float(cash_remainder)  # fi we receive non-float
+        cash_remainder = (self.limit - spent_money) / currency_rate
+        cash_remainder = round(float(cash_remainder), 2)  # reformat to x.xx
         if cash_remainder > 0:
-            return f"На сегодня осталось {round(cash_remainder, 2)} {self.currency_data[currency][1]}"
+            return f"На сегодня осталось " \
+                    f"{cash_remainder} {currency_name}"
         if cash_remainder == 0:
             return f"Денег нет, держись"
         if cash_remainder < 0:
-            return f"Денег нет, держись: твой долг - {abs(round(cash_remainder, 2))} {self.currency_data[currency][1]}"
+            return f"Денег нет, держись: твой долг - " \
+                    f"{abs(cash_remainder)} {currency_name}"
 
 
 class Record(Calculator):
@@ -61,8 +75,8 @@ class Record(Calculator):
         self.amount = amount
         self.comment = comment
         self.date = date
-        # Checking the data type of date.
-        if date is None:        # Request current time and transform it via .date method to the suitable format
+        if date is None:    # create the default time
             self.date = (dt.datetime.now()).date()
-        else:                   # Reformat string of time to the desired format
+        else:               # Reformat string of time to the desired format
             self.date = (dt.datetime.strptime(date, date_format)).date()
+
